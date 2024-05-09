22e78ff679e00ffbaf635c3370999fdb72ba4c56
Haothai1 <hao2003thai@gmail.com>
2024-05-09 02:37:47 -0700

    basic player vs. NPC

--- Snake.py
+++ Snake.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 
-# Screen
+# Screen setup
 width, height = 800, 600
 cellSize = 25
 gridW = width // cellSize
@@ -12,11 +12,11 @@ gridH = height // cellSize
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Snake")
 
-# Colors for bg/players/food
+# Colors
 black = pygame.Color(0, 0, 0)   # Background
 white = pygame.Color(255, 255, 255) # Food
-red = pygame.Color(255, 0, 0)   # Snake 1
-green = pygame.Color(0, 255, 0) # Snake 2
+red = pygame.Color(255, 0, 0)   # Snake 1 (Player)
+green = pygame.Color(0, 255, 0) # Snake 2 (AI)
 
 # Directions
 up = (0, -1)
@@ -25,99 +25,115 @@ left = (-1, 0)
 right = (1, 0)
 
 class Snake:
-    def __init__(self, color):
+    def __init__(self, color, controls):
         self.color = color
         self.pos = [self.randPos()]
         self.direction = random.choice([up, down, left, right])
+        self.controls = controls
 
     def randPos(self):
-            return (random.randint(0, gridW - 1), random.randint(0, gridH - 1))
+        return (random.randint(0, gridW - 1), random.randint(0, gridH - 1))
     
     def move(self):
         head = self.pos[0]
         dx, dy = self.direction
-        x, y = head
-        new_head = ((x + dx) % gridW, (y + dy) % gridH)
-        self.pos.insert(0, new_head)
-        self.pos.pop()
+        new_head = ((head[0] + dx) % gridW, (head[1] + dy) % gridH)
+        if new_head in self.pos[1:]:
+            display_game_over()
+        else:
+            self.pos.insert(0, new_head)
+            self.pos.pop()
+
+    def change_direction(self, key):
+        if key in self.controls:
+            new_dir = self.controls[key]
+            # Prevent reversing
+            if len(self.pos) > 1 and (self.pos[0][0] + new_dir[0], self.pos[0][1] + new_dir[1]) != self.pos[1]:
+                self.direction = new_dir
 
     def grow(self):
         self.pos.append(self.pos[-1])
 
     def draw(self):
         for segment in self.pos:
-            x, y = segment
-            pygame.draw.rect(screen, self.color, (x * cellSize, y * cellSize, cellSize, cellSize))
-    
+            pygame.draw.rect(screen, self.color, (segment[0] * cellSize, segment[1] * cellSize, cellSize, cellSize))
+
+class AISnake(Snake):
+    def __init__(self, color, food):
+        super().__init__(color, {})
+        self.food = food
+
+    def move(self):
+        head_x, head_y = self.pos[0]
+        food_x, food_y = self.food.pos
+        # Simple AI to chase food
+        if head_x < food_x:
+            self.direction = right
+        elif head_x > food_x:
+            self.direction = left
+        elif head_y < food_y:
+            self.direction = down
+        elif head_y > food_y:
+            self.direction = up
+        super().move()
+
 class Food:
     def __init__(self):
         self.pos = self.randPos()
 
     def randPos(self):
-        return (random.randint(0, gridW - 5), random.randint(0, gridH - 5))
+        return (random.randint(0, gridW - 1), random.randint(0, gridH - 1))
 
     def draw(self):
-        x, y = self.pos
-        pygame.draw.rect(screen, white, (x * cellSize, y * cellSize, cellSize, cellSize))
+        pygame.draw.rect(screen, white, (self.pos[0] * cellSize, self.pos[1] * cellSize, cellSize, cellSize))
+
+def display_game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('Game Over', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (width / 2, height / 4)
+    screen.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
 
 def main():
     clock = pygame.time.Clock()
-    snake = Snake(red)
     food = Food()
+    player_controls = {
+        pygame.K_UP: up,
+        pygame.K_DOWN: down,
+        pygame.K_LEFT: left,
+        pygame.K_RIGHT: right
+    }
+    player_snake = Snake(red, player_controls)
+    ai_snake = AISnake(green, food)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP and snake.direction != down:
-                    snake.direction = up
-                elif event.key == pygame.K_LEFT and snake.direction != right:
-                    snake.direction = left
-                elif event.key == pygame.K_DOWN and snake.direction != up:
-                    snake.direction = down
-                elif event.key == pygame.K_RIGHT and snake.direction != left:
-                    snake.direction = right
-        snake.move()
+                return
+            elif event.type == pygame.KEYDOWN:
+                player_snake.change_direction(event.key)
 
-        screen.fill(black)  # Black background following snakes movement
+        player_snake.move()
+        ai_snake.move()
 
-        # Filler game over condition
-        if snake.pos[0] in snake.pos[1:]:
-            my_font = pygame.font.SysFont('times new roman', 50)
-            
-            # creating a text surface on which text 
-            # will be drawn
-            game_over_surface = my_font.render(
-                'Game Over', True, red)
-            
-            # create a rectangular object for the text 
-            # surface object
-            game_over_rect = game_over_surface.get_rect()
-            
-            # setting position of the text
-            game_over_rect.midtop = (width/2, height/4)
-            
-            # blit will draw the text on screen
-            screen.blit(game_over_surface, game_over_rect)
-            pygame.display.flip()
-            
-            # after 2 seconds we will quit the program
-            time.sleep(2)
-            
-            # deactivating pygame library
-            pygame.quit()
-
-        if snake.pos[0] == food.pos:
-            snake.grow()
+        screen.fill(black)
+        if player_snake.pos[0] == food.pos:
+            player_snake.grow()
+            food.pos = food.randPos()
+        elif ai_snake.pos[0] == food.pos:
+            ai_snake.grow()
             food.pos = food.randPos()
 
-        snake.draw()
+        player_snake.draw()
+        ai_snake.draw()
         food.draw()
 
-        clock.tick(15)
         pygame.display.flip()
-        
+        clock.tick(15)
 
 if __name__ == "__main__":
     main()
